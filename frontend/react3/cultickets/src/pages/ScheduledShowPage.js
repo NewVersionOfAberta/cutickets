ca116225b346ea61da7a79204309d86120395886
Natali <natalia101800@gmail.com>
2021-12-15 02:16:08 +0300

    add scheduled show

--- frontend/react3/cultickets/src/pages/ScheduledShowPage.js
+++ frontend/react3/cultickets/src/pages/ScheduledShowPage.js
@@ -2,67 +2,57 @@ import React, { useEffect, useState, useContext, useCallback } from "react";
 import { useHttp } from "../hooks/http.hook";
 import { AuthContext } from "../context/AuthContext";
 import { Loader } from "../components/Loader";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 
 export const ScheduledShowPage = () => {
-  const shows1 = {
-    show: {
-      name: "Allo",
-      description: "Hi",
-      rating: "18+",
-      genres: ["Romantic", "Comedy"],
-    },
-    scheduledShows: [
-      { id: 1, date: "13:00 2022-11-10" },
-      { id: 1, date: "14:40 2021-12-12" },
-    ],
-  };
-  const [shows, setShows] = useState(shows1);
-  const { token, userId } = useContext(AuthContext);
-  const { loading, request } = useHttp();
+  const showId = useParams().id;
+  const [scheduledShows, setScheduledShows] = useState();
+  const { token } = useContext(AuthContext);
+  const { request } = useHttp();
 
-  //   const fetchShows = useCallback(async () => {
-  //     // try {
-  //     //   const fetched = await request(
-  //     //     `/shows/userId=${userId}&theatreId=0&genreId=0`,
-  //     //     "GET",
-  //     //     null,
-  //     //     {
-  //     //       Authorization: `user ${token}`,
-  //     //     }
-  //     //   );
-  //     console.log("Show1");
+  const fetchShows = useCallback(async () => {
+    try {
+      const fetched = await request(`/shows/showId=${showId}`, "GET", null, {
+        Authorization: `user ${token}`,
+      });
+      console.log("Show schedule", fetched);
 
-  //     setShows(shows1);
-  //     console.log("Show1", shows1);
-  //     // } catch (e) {}
-  //   }, [setShows]);
+      setScheduledShows(fetched);
+    } catch (e) {}
+  }, [showId, setScheduledShows]);
 
-  //   useEffect(() => {
-  //     fetchShows();
-  //   }, [fetchShows]);
+  useEffect(() => {
+    fetchShows();
+  }, [fetchShows]);
+  if (scheduledShows) {
+    console.log("Show", scheduledShows.show);
+    const dates = scheduledShows.scheduled_shows.map((e) => {
+      return (
+        <>
+          <NavLink to={`/tickets/${e.id}`}>Date: {e.datetime}</NavLink>
+          <br />
+        </>
+      );
+    });
 
-  console.log(shows);
-  const Dates = shows.scheduledShows.map((e) => {
-    <NavLink to={`/tickets/${e.id}`}>{e.date}</NavLink>;
-  });
-
-  return shows.map((e) => (
-    <>
-      <div key={e.id} className="card">
-        <div className="card-header">
-          <div className="container">
-            <div className="row">
-              <span className="col">{e.name}</span>
+    return (
+      <>
+        <div key={scheduledShows.show.id} className="card">
+          <div className="card-header">
+            <div className="container">
+              <div className="row">
+                <span className="col">{scheduledShows.show.name}</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="card-body">
-        <p className="card-text">{e.description}</p>
-        <Dates />
-      </div>
-    </>
-  ));
+        <div className="card-body">
+          <p className="card-text">{scheduledShows.show.description}</p>
+          {dates}
+        </div>
+      </>
+    );
+  }
+  return <Loader></Loader>;
   //   return showList;
 };
