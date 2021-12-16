f09e5d164f11c5c38ce61cfdee5b40f280b5c03b
Natali <natalia101800@gmail.com>
2021-12-16 03:33:34 +0300

    add profile 2

--- frontend/react3/cultickets/src/pages/Profile.js
+++ frontend/react3/cultickets/src/pages/Profile.js
@@ -5,72 +5,62 @@ import { SoldTicket } from "../components/SoldTicket";
 import { NavLink, useParams } from "react-router-dom";
 
 export const Profile = () => {
-  const showId = useParams().id;
   const [profileInfo, setProfileInfo] = useState();
-  const { token } = useContext(AuthContext);
+  const [tickets, setTickets] = useState();
+  const { token, userId } = useContext(AuthContext);
   const { request } = useHttp();
 
-  //   const fetchShows = useCallback(async () => {
-  //     try {
-  //       const fetched = await request(`/shows/showId=${showId}`, "GET", null, {
-  //         Authorization: `user ${token}`,
-  //       });
-  //       console.log("Show schedule", fetched);
+  const fetchProfileInfo = useCallback(async () => {
+    try {
+      const fetched = await request(``, "GET", null, {
+        Authorization: `user ${token}`,
+      });
+      console.log("", fetched);
 
-  //       setScheduledShows(fetched);
-  //     } catch (e) {}
-  //   }, [showId, setScheduledShows]);
+      setProfileInfo(fetched);
+    } catch (e) {}
+  }, [userId, setProfileInfo]);
 
-  //   useEffect(() => {
-  //     fetchShows();
-  //   }, [fetchShows]);
-  //   if (scheduledShows) {
-  //     console.log("Show", scheduledShows.show);
-  //     const dates = scheduledShows.scheduled_shows.map((e) => {
-  //       return (
-  //         <>
-  //           <NavLink to={`/tickets/${e.id}`}>Date: {e.datetime}</NavLink>
-  //           <br />
-  //         </>
-  //       );
-  //     });
-  const tickets = [
-    {
-      showName: "Durochka",
-      datetime: "2021-09-01",
-      status: "inactive",
-      place: 1,
-      row: 4,
-      price: 1000,
-    },
-  ];
-  const soldTickets = tickets.map((e) => {
-    return <SoldTicket>{e}</SoldTicket>;
-  });
-  return (
-    <>
-      <div className="container">
-        <section className="container">
-          <div className="card">
-            <div className="card-body">
-              <h5 id="user-name" className="card-title">
-                AndryKiller
-              </h5>
-              <div id="user-name">Name: Sergey Gavrilovitch Petrov</div>
-              <div id="user-bday">Date of birth: 20.10.1999</div>
-              <div id="user-city">Vitebsk, Belarus</div>
-              <div id="user-email">some@mail.ru</div>
-              <div class="card-footer text-muted">
-                <NavLink to={`/statistics`}>Statistics</NavLink>
+  const fetchTickets = useCallback(async () => {
+    try {
+      const fetched = await request(`/user/tickets/${userId}`, "GET", null, {
+        Authorization: `user ${token}`,
+      });
+
+      setTickets(fetched.tickets);
+    } catch (e) {}
+  }, [userId, setTickets]);
+
+  useEffect(() => {
+    fetchTickets();
+  }, [fetchTickets]);
+
+    const soldTickets = tickets.map((e) => {
+      return <SoldTicket>{e}</SoldTicket>;
+    });
+    return (
+      <>
+        <div className="container">
+          <section className="container">
+            <div className="card">
+              <div className="card-body">
+                <h5 id="user-name" className="card-title">
+                  AndryKiller
+                </h5>
+                <div id="user-name">Name: Sergey Gavrilovitch Petrov</div>
+                <div id="user-bday">Date of birth: 20.10.1999</div>
+                <div id="user-city">Vitebsk, Belarus</div>
+                <div id="user-email">some@mail.ru</div>
+                <div class="card-footer text-muted">
+                  <NavLink to={`/statistics`}>Statistics</NavLink>
+                </div>
+                <h5>Active orders:</h5>
+                {soldTickets}
               </div>
-              <h5>Active orders:</h5>
-              {soldTickets}
             </div>
-          </div>
-        </section>
-      </div>
-    </>
-  );
-  //   return <Loader></Loader>;
-  //   return showList;
+          </section>
+        </div>
+      </>
+    );
+  }
 };
